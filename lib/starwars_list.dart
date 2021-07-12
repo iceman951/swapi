69f7cf6a7d5939f7c681eb53093c723ce356fe69
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-07-12 20:17:46 +0700

    add stw list

--- lib/starwars_list.dart
+++ lib/starwars_list.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:myapp/starwars_repo.dart';
 
+class StarwarsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _StarwarsListStates();
+  }
+}
+
+
+}
