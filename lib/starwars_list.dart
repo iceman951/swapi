55f87c66b91eea379a0ad7e6906b08d2bb88442a
iceman951 <47938031+iceman951@users.noreply.github.com>
2021-07-12 20:36:06 +0700

    list

--- lib/starwars_list.dart
+++ lib/starwars_list.dart
@@ -8,5 +8,33 @@ class StarwarsList extends StatefulWidget {
   }
 }
 
+class _StarwarsListStates extends State {
+  final StarwarsRepo _repo;
+  late List<People> _people;
+  late int _page;
 
+  _StarwarsListStates() : _repo = new StarwarsRepo();
+
+  void initState() {
+    super.initState();
+    _page = 1;
+    _people = [];
+    fetchPeople();
+  }
+
+  Future<void> fetchPeople() async {
+    var people = await _repo.fetchPeople(page: _page);
+    setState(() {
+      _people = List<People>.from(people);
+      _people.addAll(people);
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: _people.length,
+        itemBuilder: (context, index) {
+          return Text(_people[index].name);
+        });
+  }
 }
